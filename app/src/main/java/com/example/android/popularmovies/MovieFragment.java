8a71f7ffd3d385a14c0a661e7845f9a491a9e0b4
Jonsnow21 <neeraj14.nj@gmail.com>
2016-07-04 22:01:02 +0530

    initialize repository

---
+++ app/src/main/java/com/example/android/popularmovies/MovieFragment.java
@@ -0,0 +1,11 @@
+package com.example.android.popularmovies;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by neeraj on 4/7/16.
+ */
+public class MovieFragment extends Fragment {
+    public MovieFragment(){
+    }
+}
