e9159db5629f1a10e0834278327b0f4a2c65aca7
Jonsnow21 <neeraj14.nj@gmail.com>
2016-07-16 15:35:12 +0530

    sort by rating added

--- app/src/main/java/com/example/android/popularmovies/MovieFragment.java
+++ app/src/main/java/com/example/android/popularmovies/MovieFragment.java
@@ -1,5 +1,9 @@
 package com.example.android.popularmovies;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -7,10 +11,15 @@ import android.support.v4.app.Fragment;
 import android.support.v4.os.AsyncTaskCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -22,6 +31,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.Inflater;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -29,33 +39,69 @@ import javax.net.ssl.HttpsURLConnection;
 public class MovieFragment extends Fragment {
 
     private final String LOG_TAG = MovieFragment.class.getSimpleName();
+    private String sortOrder;
 
     public MovieFragment(){
     }
 
-    private ArrayAdapter<String> myAdapter;
+    private MovieAdapter myAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate( savedInstanceState);
+
+        setHasOptionsMenu( true );
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.main,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if( id == R.id.sort_by_most_popular ){
+            sortOrder = "/popular?";
+            updateMovies( sortOrder);
+            return true;
+        } else if( id == R.id.sort_by_top_rated ){
+            sortOrder = "/top_rated?";
+            updateMovies(sortOrder);
+            return true;
+        }
+        return super.onOptionsItemSelected( item );
     }
 
     @Override
     public void onStart(){
         super.onStart();
-        FetchMovies fetchMovies = new FetchMovies();
-        fetchMovies.execute();
-        Log.v( LOG_TAG, "in on start");
+        sortOrder = "/popular?";
+        updateMovies( sortOrder );
+    }
+
+    public void updateMovies( String order){
+        if( isOnline() ) {
+            FetchMovies fetchMovies = new FetchMovies();
+            fetchMovies.execute( order );
+            Log.v(LOG_TAG, "in on start");
+        } else {
+            Toast.makeText(getContext(),"Please turn on an active internet connection",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean isOnline(){
+        ConnectivityManager cm = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        return info != null && info.isConnectedOrConnecting();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
-        myAdapter = new ArrayAdapter<String>(
+        myAdapter = new MovieAdapter(
                 getActivity(),
-                R.layout.grid_item,
-                R.id.movie_poster,
-                new ArrayList<String>()
+                new ArrayList<Movie>()
         );
 
         View rootView = inflater.inflate( R.layout.fragment_main, container, false);
@@ -63,21 +109,28 @@ public class MovieFragment extends Fragment {
         GridView gridView = (GridView) rootView.findViewById(R.id.grid_for_posters);
         gridView.setAdapter( myAdapter );
 
+        gridView.setOnItemClickListener( new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Movie movie = myAdapter.getItem( position);
+                //Toast.makeText( getContext(), "Movie " + movie.getTitle() + " Selected", Toast.LENGTH_SHORT ).show();
+
+                Intent intent = new Intent( getActivity(), DetailActivity.class).putExtra(Intent.EXTRA_TEXT, movie);
+                startActivity(intent);
+            }
+        });
+
         return rootView;
     }
 
-    public class FetchMovies extends AsyncTask< Void, Void, String >{
+    public class FetchMovies extends AsyncTask< String, Void, String >{
 
         private final String LOG_TAG = FetchMovies.class.getSimpleName();
 
         @Override
-        protected String doInBackground(Void... params) {
+        protected String doInBackground(String... params) {
 
             Log.v( LOG_TAG, "dude doInBackground");
-
-//            if( params.length == 0 ){
-//                return null;
-//            }
 
             String JSONStr = null;
 
@@ -85,7 +138,7 @@ public class MovieFragment extends Fragment {
             BufferedReader reader = null;
 
             try{
-                final String BASE_URL = "https://api.themoviedb.org/3/movie/popular?";
+                final String BASE_URL = "https://api.themoviedb.org/3/movie" + params[0];
                 final String APIID_PARAM = "api_key";
 
                 Uri builtUri = Uri.parse( BASE_URL).buildUpon()
@@ -152,26 +205,26 @@ public class MovieFragment extends Fragment {
             final String TMDB_OVERVIEW = "overview";
 
             try {
-                myAdapter.clear();
-
                 JSONObject json = new JSONObject( jsonString );
                 JSONArray jsonArray = json.optJSONArray( TMDB_RESULTS );
 
-                String resultStr[] = new String[ jsonArray.length()];
+                myAdapter.clear();
+
+                Movie moviesArray[] = new Movie[jsonArray.length()];
 
 
                 for( int i = 0; i < jsonArray.length(); i++ ){
                     JSONObject jsonObject = jsonArray.getJSONObject(i);
 
-                    String originalTile = jsonObject.getString( TMDB_ORIGINAL_TITLE);
+                    String originalTitle = jsonObject.getString( TMDB_ORIGINAL_TITLE);
                     String posterPath = jsonObject.getString( TMDB_POSTER_PATH);
                     String releaseDate = jsonObject.getString( TMDB_RELEASE_DATE);
-                    String userRating = jsonObject.getString( TMDB_USER_RATING);
+                    float userRating = Float.parseFloat(jsonObject.getString( TMDB_USER_RATING));
                     String overView = jsonObject.getString( TMDB_OVERVIEW);
 
-                    resultStr[i] = originalTile + posterPath + releaseDate + userRating + overView;
+                    moviesArray[i] = new Movie( originalTitle, posterPath, releaseDate, userRating, overView );
 
-                    myAdapter.add( resultStr[i] );
+                    myAdapter.add(moviesArray[i]);
                 }
             } catch ( JSONException j ){
                 j.printStackTrace();
